ed1a70304bab5a44694b45d79096e81f48986621
Yuvraj004 <yuvrajchat@gmail.com>
2024-07-01 21:31:17 +0530

    classification

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
-import { Button } from "@/components/ui/button";
+import { Button, buttonVariants } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
 import Image from "next/image";
 import Link from "next/link";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-start p-24">
-      <Link href={'/image-classification'}> Upload Image</Link>
+      <Link className={cn(buttonVariants({variant:'outline'}))} href={'/image-classification'}> Upload Image</Link>
     </main>
   );
 }
